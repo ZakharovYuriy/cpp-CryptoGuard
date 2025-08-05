3f9229c163e81cde493ed65b6b61d1ddad05423f
ZakharovYuriy <zakharov.yura13@yandex.ru>
2025-08-06 01:29:46 +0200

    fixed main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,68 @@
 #include "cmd_options.h"
 #include "crypto_guard_ctx.h"
-#include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <openssl/evp.h>
 #include <print>
 #include <stdexcept>
 #include <string>
 
+std::ifstream openInputBinaryFile(const std::string& filename) {
+    std::ifstream inFile{filename, std::ios::binary};
+    if (!inFile.is_open() || inFile.fail())
+        throw std::runtime_error(
+            "Cannot open file for reading: " + filename);
+    return inFile;
+}
+
+std::ofstream openOutputBinaryFile(const std::string& filename) {
+    std::ofstream outFile{filename, std::ios::binary};
+    if (!outFile.is_open() || outFile.fail())
+        throw std::runtime_error(
+            "Cannot open file for writing: " + filename);
+    return outFile;
+}
+
 int main(int argc, char *argv[]) {
     try {
         CryptoGuard::ProgramOptions programOptions(argc, argv);
-        
-        return EXIT_SUCCESS;
-    } catch (const std::exception& e) {
-        std::cout << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
-    try {
-       
-
-        CryptoGuard::ProgramOptions options(argc, argv);
-
         CryptoGuard::CryptoGuardCtx cryptoCtx;
-
         using COMMAND_TYPE = CryptoGuard::ProgramOptions::COMMAND_TYPE;
-        switch (options.GetCommand()) {
+        switch (programOptions.GetCommand()) {
         case COMMAND_TYPE::ENCRYPT:
+        {
+            auto inStream = openInputBinaryFile(programOptions.GetInputFile());
+            auto outStream = openOutputBinaryFile(programOptions.GetInputFile());
+            cryptoCtx.EncryptFile(inStream, outStream, programOptions.GetPassword());
             std::print("File encoded successfully\n");
             break;
+        }
 
         case COMMAND_TYPE::DECRYPT:
+        {
+            auto inStream = openInputBinaryFile(programOptions.GetInputFile());
+            auto outStream = openOutputBinaryFile(programOptions.GetInputFile());
+            cryptoCtx.DecryptFile(inStream, outStream, programOptions.GetPassword());
             std::print("File decoded successfully\n");
             break;
+        }
 
         case COMMAND_TYPE::CHECKSUM:
-            std::print("Checksum: {}\n", "CHECKSUM_NOT_IMPLEMENTED");
+        {
+            auto inStream = openInputBinaryFile(programOptions.GetInputFile());
+            std::print("Checksum: {}\n", cryptoCtx.CalculateChecksum(inStream));
             break;
+        }
 
         default:
             throw std::runtime_error{"Unsupported command"};
         }
-
-    } catch (const std::exception &e) {
+        
+        return EXIT_SUCCESS;
+    } catch (const std::exception& e) {
         std::print(std::cerr, "Error: {}\n", e.what());
-        return 1;
+        return EXIT_FAILURE;
     }
-
-    return 0;
+    
+    return EXIT_SUCCESS;
 }
